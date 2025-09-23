da17c22ebb7962869432597c1d6326646034bbc1
Jeff Ouda <jeffoudahabakuk@gmail.com>
2025-09-23 23:57:21 +0300

    main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+const recipe = {
+  id: 1,
+  name: "Spaghetti Carbonara",
+  image: "https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg",
+  ingredients: ["Spaghetti", "Eggs", "Pancetta", "Parmesan"],
+  instructions:
+    "Boil pasta, fry pancetta, mix eggs and cheese, combine everything.",
+};
+
+const recipesList = document.getElementById("recipes-list");
+
+function renderRecipeCard(recipe) {
+  const card = document.createElement("div");
+  card.classList.add("recipe-card");
+}
