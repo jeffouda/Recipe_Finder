31e986e944f2d93fc500e716260f3eb28c859f86
Jeff Ouda <jeffoudahabakuk@gmail.com>
2025-09-24 19:50:07 +0300

    main.js

--- main.js
+++ main.js
@@ -1,21 +1,8 @@
-const recipe = {
-  id: 1,
-  name: "Spaghetti Carbonara",
-  image: "https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg",
-  ingredients: ["Spaghetti", "Eggs", "Pancetta", "Parmesan"],
-  instructions:
-    "Boil pasta, fry pancetta, mix eggs and cheese, combine everything.",
-};
-
+//
 const recipesList = document.getElementById("recipes-list");
-
-function renderRecipeCard(recipe) {
-  const card = document.createElement("div");
-  card.classList.add("recipe-card");
-
-  card.innerHTML = `<img src="${recipe.image}" alt="${recipe.name}">
-    <h3>${recipe.name}</h3>`;
-
-  recipesList.appendChild(card);
-}
-renderRecipeCard(recipe);
+const detailsSection = document.getElementById("recipe-details");
+const detailsBody = document.getElementById("details-body");
+const closeBtn = document.getElementById("close-details");
+const favoritesList = document.getElementById("favorites-list");
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search-input");
